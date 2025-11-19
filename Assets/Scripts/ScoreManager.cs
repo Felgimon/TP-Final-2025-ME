e9f40d7c65a41f5c5593104da4e276e64585c300
Felgi <felipeferraridoval@gmail.com>
2025-11-18 22:45:20 -0300

    Score manager working

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreTMP;
+    [SerializeField] int score = 0;
+    [SerializeField] int maxScore = 10;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        scoreTMP.text = score.ToString();
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            addScore(1);
+        }
+        if (score  == maxScore)
+        {
+            score = 0;
+        }
+    }
+
+    private void addScore(int points)
+    {
+        score += points;
+    }
+}
