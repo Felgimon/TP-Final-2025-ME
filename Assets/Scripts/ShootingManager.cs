3c5aacb7d1f86a03eac5473fd16e555816623374
Felgi <felipeferraridoval@gmail.com>
2025-11-27 16:35:19 -0300

    Raycast working y preparación de escena para el baking

--- Assets/Scripts/ShootingManager.cs
+++ Assets/Scripts/ShootingManager.cs
@@ -21,18 +21,21 @@ public class ShootingManager : MonoBehaviour
             GameObject bullet = BulletPool.Instance.RequestBullet();
             //Configuraciones pq el rb es malardo, deberia de haberlo hecho con velocity, maldito anunciado.
             bullet.transform.position = transform.position + transform.forward * bulletOffSet;
+            RaycastHit hit;
+            Vector3 direccion = transform.forward; //por default es la dirección del gunpoint
+            if (Physics.Raycast(cameraTr.position, cameraTr.forward, out hit, rayDistance))
+            {
+                //Debug.Log("colision raycast:" + hit.point);
+                direccion = Vector3.Normalize(hit.point - bullet.transform.position); //Si colisiona con algo la direccion del bullet va hacia la crosshair
+            }
+            Gizmos.color = Color.yellow;
             bullet.transform.rotation = transform.rotation * Quaternion.Euler(0, -90f, 0); //Misma rotacion que el gunpoint solo que le cambio el "y" para que el metal del dardo apunte bien.
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             rb.velocity = Vector3.zero; //Lo reinicio antes de ponerle el addForce para que no se llegue a stackear de los addForces de antes
             rb.angularVelocity = Vector3.zero;
-            rb.AddForce(transform.forward * bulletSpeed, ForceMode.VelocityChange); //Problema con el el addforce, salen a cualquier lado.
-        }
-        RaycastHit hit;
-        if (Physics.Raycast(cameraTr.position, cameraTr.forward, out hit, rayDistance)) {
-             //Debug.Log("colision raycast:" + hit.point);
+            rb.AddForce(direccion * bulletSpeed, ForceMode.VelocityChange); //Problema con el el addforce, salen a cualquier lado.
         }
 
-        Gizmos.color = Color.yellow;
         //Debug.DrawRay(cameraTr.position, cameraTr.forward * rayDistance, Gizmos.color);
     }
 }
